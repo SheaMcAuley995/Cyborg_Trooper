4940685dfb6d81c243cfea81f953fce13f25671c
Unknown <s179060@students.aie.edu.au>
2018-02-27 09:12:30 -0800

    Health bar

---
+++ Assets/EvanAssets/Scripts/DamageTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageTrigger : MonoBehaviour
+{
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            damaging();
+        }
+    }
+
+    void damaging()
+    {
+        Debug.Log("Damaging");
+    }
+}
---
+++ Assets/EvanAssets/Scripts/HealthBar.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class HealthBar : MonoBehaviour
+{
+
+    public Image currentHealthBar;
+    public Text healthValue;
+
+    public float hitpoint = 150.0f;
+    private float maxHitpoint = 150.0f;
+
+    // Use this for initialization
+    void Start()
+    { 
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float ratio = hitpoint / maxHitpoint;
+        currentHealthBar.rectTransform.localScale = new Vector3(ratio, 1, 1);
+        healthValue.text = (ratio * 100).ToString("0") + '%';
+        if (hitpoint <= 0)
+        {
+            Debug.Log("Player killed");
+        }
+        if (hitpoint >= maxHitpoint)
+        {
+            hitpoint = maxHitpoint;
+            Debug.Log("Player at max health");
+        }
+    }
+
+    void TakeDamage(float damage)
+    {
+        if (hitpoint < 0)
+        {
+            hitpoint -= damage;
+        }
+     }
+
+    void HealDamage(float heal)
+    {
+        if (hitpoint < maxHitpoint)
+        {
+            hitpoint += heal;
+        }
+    }
+}
