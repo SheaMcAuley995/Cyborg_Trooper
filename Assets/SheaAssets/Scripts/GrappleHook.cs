f7c4e88d5ea9d77520cedc5166d87dd1c559e8fb
Shea McAuley <s179049@students.aie.edu.au>
2018-02-27 13:00:04 -0800

    Grappling lerped]

--- Assets/SheaAssets/Scripts/GrappleHook.cs
+++ Assets/SheaAssets/Scripts/GrappleHook.cs
@@ -4,66 +4,10 @@ using UnityEngine;
 
 public class GrappleHook : MonoBehaviour {
 
-    public Camera cam;
-    public GameObject hook;
 
-
-    public float fallMultiplier = 2.5f;
-    public float lowJumpMultiplier = 2f;
-    Rigidbody2D rb;
-
-    public float jumpVelocity;
-    bool jumpRequest;
-
-    private void Awake()
+    private void OnCollisionEnter2D(Collision2D c)
     {
-        rb = GetComponent<Rigidbody2D>();
+        
     }
 
-    void Update()
-    {
-
-    }
-
-    void FixedUpdate()
-    {
-        if(rb.velocity.y < 0)
-        {
-            rb.gravityScale = fallMultiplier;
-            //rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
-        }
-        else if (rb.velocity.y > 0 && !Input.GetButton("Jump"))
-        {
-            rb.gravityScale = lowJumpMultiplier;
-            //rb.velocity += Vector2.up * Physics2D.gravity.y * 
-        }
-        else
-        {
-            rb.gravityScale = 1f;
-        }
-
-    }
-
-    private void UpdateInput()
-    {
-
-        if (Input.GetMouseButtonDown(0))
-        {
-            // Find mouse position
-            Vector3 mouseInput = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10);
-            Vector2 mouseClick = cam.ScreenToWorldPoint(mouseInput);
-
-
-            //Find direction (ray)
-            Vector3 rayDirection = mouseClick - (Vector2)this.transform.position;
-            Vector3 rayNormalised = rayDirection.normalized;
-            Vector3 r = Quaternion.Euler(0, 90, 0) * rayNormalised;
-            Instantiate(hook, transform.position, Quaternion.Euler(r), transform);
-
-           // RaycastHit2D hit = Physics2D.Raycast((Vector2)this.transform.position, rayDirection, grapple.grapplingHookRange, ~(1 << grapple.playerLayer));
-            
-        }
-
-        }
-
-    }
+}
--- Assets/SheaAssets/Scripts/JumpBox.cs
+++ Assets/SheaAssets/Scripts/JumpBox.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 
 
-[RequireComponent(typeof(LineRenderer))]
-[RequireComponent(typeof(Rigidbody2D))]
+//[RequireComponent(typeof(LineRenderer))]
+//[RequireComponent(typeof(Rigidbody2D))]
 
 public class JumpBox : MonoBehaviour {
 
@@ -19,9 +19,12 @@ public class JumpBox : MonoBehaviour {
         //Move Speed
     [Range(1, 10)]
     public float characterSpeed;
+    [Range(1, 3)]
+    public float hookLength;
+    [Range(0, 0.4f)]
+    public float travelTime = 0.125f;
 
-
-        //Collision Detection
+    //Collision Detection
     Rigidbody2D playerRb;
     public LayerMask mask;
 
@@ -39,7 +42,11 @@ public class JumpBox : MonoBehaviour {
     public GameObject hook;
     Rigidbody2D hookRb;
 
-
+    GameObject hookObj;
+    Vector3 hookStart;
+    Vector3 hookEnd;
+    bool isHooking;
+    public float hookSpeed;
     //
     //END ROPE MECHANICS
     //
@@ -68,7 +75,7 @@ public class JumpBox : MonoBehaviour {
         {
             jumpRequest = true;
         }
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && !isHooking)
         {
             // Find mouse position
             Vector3 mouseInput = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10);
@@ -81,9 +88,12 @@ public class JumpBox : MonoBehaviour {
             Vector3 r = Quaternion.Euler(0, 90, 0) * rayNormalised;
             GameObject baby = Instantiate(hook, transform.position + (rayDirection.normalized * 1.2f), Quaternion.Euler(r));
             hookRb = baby.GetComponent<Rigidbody2D>();
+            hookObj = baby;
+            isHooking = true;
+            hookStart = baby.transform.position;
+            hookEnd =  baby.transform.position + (Vector3)(mouseClick.normalized * hookLength);
 
-
-
+            StartCoroutine(moveHook());
             //hookRb.AddForce(rayDirection.normalized * 25, ForceMode2D.Impulse);
             
         }
@@ -91,6 +101,7 @@ public class JumpBox : MonoBehaviour {
     }
     private void FixedUpdate()
     {
+
         if (jumpRequest)
         {
             GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpVelocity, ForceMode2D.Impulse);
@@ -103,7 +114,10 @@ public class JumpBox : MonoBehaviour {
             Vector2 boxCenter = (Vector2)transform.position + Vector2.down * (playerSize.y + boxSize.y) * 0.05f;
             grounded = (Physics2D.OverlapBox(boxCenter,boxSize,0f,mask) != null);
         }
-
+        //if(isHooking)
+        //{
+        //   // LerpBaby(hookStart, hookEnd);
+        //}
         float h = Input.GetAxis("Horizontal");
         Move(h);
     }
@@ -113,10 +127,39 @@ public class JumpBox : MonoBehaviour {
         playerRb.velocity = new Vector2(speed * characterSpeed, playerRb.velocity.y);
     }
 
-    private void LerpBaby(Transform start, Transform end)
+    //private void LerpBaby(Vector3 start, Vector3 end)
+    //{
+    //    if(hookObj != null)
+    //    {
+    //        hookObj.transform.position = Vector3.Lerp(start, end, hookSpeed * Time.deltaTime);
+            
+    //    }
+    //}
+
+    IEnumerator moveHook()
     {
+       Collider2D c = hookObj.GetComponent<BoxCollider2D>();
+        float t = 0;
+        while(t < 1)//whatever parameter you want
+        {
+
+            //Vector3 B_start = hookStart;
+            //Vector3 B_end = hookEnd;
+            t += Time.deltaTime / travelTime;
+            hookObj.transform.position = Vector3.Lerp(hookStart, hookEnd, t);
+            Debug.Log(t);
+            
+
+            yield return null;
+
+            
+
+        }
+
+        Destroy(hookObj);
+        isHooking = false;
+        //Cleanup after you leave the while loop
+
 
     }
-
-
 }
