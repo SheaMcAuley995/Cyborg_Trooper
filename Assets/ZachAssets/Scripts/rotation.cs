d12b11221f8a96122d5931e8ac55d8dfe5a56d0c
Zach Oriel <Zacharyaaron15@gmail.com>
2018-02-26 17:24:05 -0800

    More level design

---
+++ Assets/ZachAssets/Scripts/rotation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotation : MonoBehaviour {
+
+    [Header("Rotate Speeds")]
+    [Range(-25,25)]
+    public float rotationSpeedX;
+    [Range(-25, 25)]
+    public float rotationSpeedY;
+    [Range(-25, 25)]
+    public float rotationSpeedZ;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Rotate(rotationSpeedX, rotationSpeedY, rotationSpeedZ);
+    }
+}
